adf335f704d113ff7f0296e3dee2113d04d5f863
Justin Thomas <ThomasCJustin@gmail.com>
2018-02-07 10:32:36 -0500

    Implement Initial Game Logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,41 @@
+let doors = document.getElementsByClassName('door');
 
+let doorArray = [];
+
+for (let i = 0; i < doors.length; i++) {
+  doorArray.push({
+    doorNumber: i + 1,
+    prize: "N/A"
+  });
+}
+
+init();
+
+function init() {
+  chooseCarDoor();
+  addDoorEvents();
+}
+
+function chooseCarDoor() {
+  let carDoorNumber = Math.floor(Math.random() * doors.length);
+
+  for (let i = 0; i < doors.length; i++) {
+    if (carDoorNumber === i) {
+      doorArray[i].prize = "Car";
+    } else {
+      doorArray[i].prize = "Goat";
+    }
+  }
+}
+
+function addDoorEvents()  {
+  for (let i = 0; i < doors.length; i++) {
+    doors[i].addEventListener('click', function() {
+      if (doorArray[i].prize === "Car") {
+        console.log("You win a brand new car!");
+      } else {
+        console.log("So Sad, but you get to take home this goat!");
+      }
+    });
+  }
+}
